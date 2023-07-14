660ffce3fa5db0efea2174802fee95b4079f8d61
Sabrina Montoya <sabri2056@gmail.com>
2023-07-13 22:28:36 -0300

    terceraentrega

--- js/app.js
+++ js/app.js
@@ -1,90 +1,160 @@
 console.log("conectado");
 
-function saludarYPromos() {
-  let respuesta = prompt("Bienvenido!!! ¿Deseas saber las promos del mes? (Responde 'Sí' o 'No')");
 
-  if (respuesta == "si") {
-    console.log("%cAquí están las promos del mes: Con la compra de Combos recibes una bebida gratis!!", "color: red");
-    console.log("%c- Descuento del 10% si retiras el pedido", "color: red");
-  } else {
-    console.log("%cGracias Que tengas un buen día", "color: blue");
+class Carrito {
+  constructor() {
+    this.productos = new Map();
   }
+  Eliminar() {
+    this.productos = new Map();
+  }
+
+  Guardar() {
+    localStorage.setItem(
+      "carrito",
+      JSON.stringify(Object.fromEntries(this.productos))
+    );
+  }
+
+  Cargar() {
+    if (localStorage.getItem("carrito") != null) {
+      this.productos = new Map(
+        Object.entries(JSON.parse(localStorage.getItem("carrito")))
+      );
+    } else {
+      this.productos = new Map();
+    }
+  }
+
+  Agregar(id, cantidad) {
+    let cantidadNew = cantidad;
+    if (this.productos.has(id)) {
+      cantidadNew += this.productos.get(id);
+    }
+    this.productos.set(id, cantidadNew);
+  }
+   
 }
+let newCarrit = new Carrito ()
+newCarrit.Cargar();
 
 
-
-saludarYPromos();
 
 
 class Producto {
-  constructor(nombre, precio, cantidad) {
+  constructor(id, nombre, precio, cantidad) {
+    this.Id = id;
     this.Nombre = nombre;
     this.Precio = precio;
     this.Cantidad = cantidad;
-    this.disponible = true;
   }
-  estaDisponible(){
+
+  estaDisponible() {
     return this.Cantidad > 0;
+  }
+  mostrar() {
+    if (this.estaDisponible()) {
+      console.log(
+        `%c${this.Nombre} - Precio: %c${this.Precio} - Cantidad: %c${this.Cantidad}`,
+        "font-weight: bold;",
+        "color: red;",
+        "color: salmon;"
+      );
+    } else {
+      console.log(
+        `%c${this.Nombre} (No disponible)`,
+        "font-weight: bold; color: red;"
+      );
+    }
+  }
+}
+
+//Array
+const listaDeProductos = [
+  new Producto(1, "Bolitas de salmón con queso saborizado", 1500, 100),
+  new Producto(2, "Langostinos rebozados", 600, 100),
+  new Producto(3, "Empanada china", 300, 100),
+  new Producto(4, "Hot Rolls - New York", 3500, 10),
+  new Producto(5, "Hot Rolls - Philadelphia", 2500, 60),
+  // new Producto(6, "Hot Rolls - Shrimp", 4500, 100),
+  // new Producto(7, "Hot Rolls - Veggi", 2500, 50),
+  // new Producto(8, "Hot Rolls - Honey", 3200, 80),
+  // new Producto(9, "Viandas - Personalizada", 1500, 75),
+  // new Producto(10, "Viandas - Ejecutivo", 1500, 5),
+  // new Producto(11, "Viandas - Tradicional", 1500, 35),
+  // new Producto(12, "Viandas - Opcion del dia", 1500, 100),
+  // new Producto(13, "Combos - Salmon, Langostinos, Kanikama", 5500, 100),
+  // new Producto(14, "Combos - Salmon, Langostinos", 6500, 90),
+  // new Producto(15, "Combos - Todo Tamago", 7500, 25),
+  // new Producto(15, "Combos - Premiun", 8500, 65),
+  // new Producto(16, "Combos - Todo Salmon", 7500, 28),
+  // new Producto(17, "Combos - Vegui", 6500, 0),
+];
+
+
+// Obtén la lista de productos del DOM
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén la lista de botones "Agregar al carrito"
+  const btnAddToCart = document.querySelectorAll(".btn-add-cart");
+
+  // Agrega un evento click a cada botón
+  btnAddToCart.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      newCarrit.Cargar();
+      
+
+      const id = btn.getAttribute("data-id");
+    newCarrit.Agregar(id,1 );
+    newCarrit.Guardar();
+
+      console.log("Producto agregado al carrito:", newCarrit);
+    });
+  });
+});
+
+
+
+// Mostrar los productos en el carrito
+function mostrarCarritoModal() {
+  const cartItems = document.getElementById("cartItems");
+  cartItems.innerHTML = "";
+
+  newCarrit.productos.forEach(idproducto => 
+
+  {
+    const producto = listaDeProductos.find(prod => prod.Id === idproducto)
     
-  }
-  mostrar(){
-    if (this.estaDisponible ()) {
-      console.log(`%c${this.Nombre} - Precio: %c${this.Precio} - Cantidad: %c${this.Cantidad}`, "font-weight: bold;", "color: red;", "color: salmon;");
-    } else {
-      console.log(`%c${this.Nombre} (No disponible)`, "font-weight: bold; color: red;");
-    }
-  }
+    const itemCarrito = document.createElement("div");
+    itemCarrito.classList.add("item-carrito");
+    itemCarrito.innerHTML = `
+      <h5>${producto.Nombre}</h5>
+      <p>Precio: $${producto.Precio} | Cantidad: ${newCarrit.productos.get(idproducto.toString())}</p>
+    `;
+    cartItems.appendChild(itemCarrito);
+    const separador = document.createElement("hr");
+    cartItems.appendChild(separador);
+  });
 }
 
-const listaDeProductos = [];
+//Evento para abrir el modal del carrito al hacer clic en el botón "Ver carrito"
+const btnVerCarrito = document.querySelector(
+'.btn.btn-primary[data-bs-target="#modal"]'
+);
+btnVerCarrito.addEventListener("click", () => {
+  mostrarCarritoModal();
+});
 
-listaDeProductos.push(new Producto("Bolitas de salmón con queso saborizado", 1500, 100));
-listaDeProductos.push(new Producto("Langostinos rebozados", 600, 100));
-listaDeProductos.push(new Producto("Empanada china", 300, 100));
-listaDeProductos.push(new Producto("Hot Rolls - New York", 3500, 10));
-listaDeProductos.push(new Producto("Hot Rolls - Philadelphia", 2500, 60));
-listaDeProductos.push(new Producto("Hot Rolls - Shrimp", 4500, 100));
-listaDeProductos.push(new Producto("Hot Rolls - Veggi", 2500, 50));
-listaDeProductos.push(new Producto("Hot Rolls - Honey", 3200, 80));
-listaDeProductos.push(new Producto("Viandas - Personalizada", 1500, 75));
-listaDeProductos.push(new Producto("Viandas - Ejecutivo", 1500, 5));
-listaDeProductos.push(new Producto("Viandas - Tradicional", 1500, 35));
-listaDeProductos.push(new Producto("Viandas - Opcion del dia", 1500, 100));
-listaDeProductos.push(new Producto("Combos - Salmon, Langostinos, Kanikama", 5500, 100));
-listaDeProductos.push(new Producto("Combos - Salmon, Langostinos", 6500, 90));
-listaDeProductos.push(new Producto("Combos - Todo Tamago", 7500, 25));
-listaDeProductos.push(new Producto("Combos - Premiun", 8500, 65));
-listaDeProductos.push(new Producto("Combos - Todo Salmon", 7500, 28));
-listaDeProductos.push(new Producto("Combos - Vegui", 6500, 0));
-
-console.log("%cLista de productos disponibles:", "color: green; font-weight: bold;");
-
-for (let i = 0; i < listaDeProductos.length; i++) {
-  let producto = listaDeProductos[i];
-  producto.mostrar();
-
-}
+const btnborrarCarrito = document.getElementById(
+  'borrar'
+  );
+  btnborrarCarrito.addEventListener("click", () => {
+   newCarrit.Eliminar()
+   newCarrit.Guardar()
+    mostrarCarritoModal();
+  });
 
 
-function buscarProducto() {
-  let buscar = prompt("¿Cuál es tu antojo del día?");
 
-  if (buscar.length > 0) {
-    let encontrados = listaDeProductos.filter(prod => prod.Nombre.includes(buscar));
-    if (encontrados.length > 0) {
-      console.log("Encontramos los siguientes productos:");
-      encontrados.forEach(producto => {
-        console.log(producto.Nombre);
-      });
-    } else {
-      console.log("Producto no encontrado");
-    }
-  }
-}
 
-buscarProducto();
-  
-  
-  
-  
-  
+
